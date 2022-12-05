6a7accb5226b804fa1e221471a31ca65f837893b
Анастасия Каплуновская <nastya.anna2013@yandex.ru>
2022-12-05 20:33:58 +0300

    Added new file

---
+++ yatube/posts/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group 
+
+def index(request):
+    template = 'posts/index.html'
+    title = 'Последние объявления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts':posts,
+        'title': title,
+        'text':'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = f'Записи сообщества {slug}'
+    group = get_object_or_404 (Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'text': 'Лев Толстой - зеркало русской души ',
+        'group': group,
+        'posts': posts,  
+    }
+    return render(request, template, context) 
+
+
+
+
